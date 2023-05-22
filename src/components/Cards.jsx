b43c4c6ee93d7b273a278a1f1ef75e4785c3d22e
jamatov <mirabdullajamatov@gmail.com>
2023-05-22 11:13:26 +0500

    s

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -12,7 +12,7 @@ export default function Cards() {
             <div data-aos="zoom-in" data-aos-duration="1500" className="card">
               <img src={icon1} alt="" />
               <h3>Orginal maxsulotlar</h3>
-              <p></p>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,</p>
             </div>
           </div>
 
